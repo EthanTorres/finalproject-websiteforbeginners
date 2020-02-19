251d24f1b5fdd45fcf06c8f5b45305ab2b72e6e3
yeetrr <60768468+yeetrr@users.noreply.github.com>
2020-02-18 18:06:11 -0800

    php fix

---
+++ webserver/server.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Thank you for contacting us, your email to check for infomation is: <?php echo $_POST["email"]; ?><br>
+We will respond to your infomation that you said:  <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
