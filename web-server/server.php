09ab1b8aefb0f07212ee237f4fda0d0ef0ec7728
yeetrr <60768468+yeetrr@users.noreply.github.com>
2020-02-18 17:39:57 -0800

    php server

---
+++ web-server/server.php
@@ -0,0 +1,5 @@
+<html>
+<body>
+    Thank you for contacting us, check your emails on <?php echo $_GET["email"]; ?><br>We will respond to your infomation, <?php echo $_GET["info"]; ?>
+</body>
+</html>
