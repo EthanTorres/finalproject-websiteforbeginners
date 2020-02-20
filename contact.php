8980045704b2dfc5faf7ac2499557d2e43bf4f19
yeetrr <60768468+yeetrr@users.noreply.github.com>
2020-02-19 16:01:30 -0800

    added php folder

--- contact.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-
-Your email address to check is: <?php echo $_POST["email"]; ?><br>
-We will respond to your message: <?php echo $_POST["info"]; ?>
-
-</body>
-</html>
---
+++ php/contact.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    Your email address to check is: <?php echo $_POST["email"]; ?><br>
+    We will respond to your message: <?php echo $_POST["info"]; ?> 
+    </body>
+</html>
