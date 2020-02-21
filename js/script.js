e13ecad67a88fcc24310e557526c67ec43733f1b
yeetrr <60768468+yeetrr@users.noreply.github.com>
2020-02-20 18:49:54 -0800

    js add

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ window.dataLayer = window.dataLayer || [];
   gtag('js', new Date());
   gtag('config', 'UA-54516992-1');
 //  Google Analytics!
+
+function DarkMode() {
+  document.body.style.backgroundColor = "#121212"
+}
