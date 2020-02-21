de9f17baef6de18813c56a48d0d36634be3674bb
yeetrr <60768468+yeetrr@users.noreply.github.com>
2020-02-20 19:35:30 -0800

    js work

--- js/script.js
+++ js/script.js
@@ -4,26 +4,18 @@ window.dataLayer = window.dataLayer || [];
   gtag('config', 'UA-54516992-1');
 //  Google Analytics!
 
-$("#darkmode").click(function(){
-  var text = $("text");
-  var background = document.body
-  var h1 = $("h1")
-  var h2 = $("h2")
-  var h3 = $("h3")
-  var h4 = $("h4")
-  var h5 = $("h5")
-  var h6 = $("h6")
-  var p = $("p")
-  var a = $("a")
-  text.show();
-  background.style.backgroundColor = "#121212";
-  h1.color = "white";
-  h2.color = "white";
-  h3.color = "white";
-  h4.color = "white";
-  h5.color = "white";
-  h6.color = "white";
-  p.color = "white";
-  a.color = "white";
+$(document).ready(function() {
+   $("#btn").click(function() {
+      $("#text").show();
+      $('h1').css("color","white");
+      $('h2').css("color","white");
+      $('h3').css("color","white");
+      $('h4').css("color","white");
+      $('h5').css("color","white");
+      $('h6').css("color","white");
+      $('p').css("color","white");
+      $('body').css("background-color", "#121212");
+      $('navbar-expand-sm').css("color", "#121212");
+   });
 });
 // Dark Mode
